1abd497d17f3651c1d9e45c34011fc22890141f5
Marcelo Lara <chelop@gmail.com>
2022-05-16 15:56:53 -0400

    Adding the database

--- lib/todo_application-stack.ts
+++ lib/todo_application-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class TodoApplicationStack extends cdk.Stack {
@@ -18,6 +19,19 @@ export class TodoApplicationStack extends cdk.Stack {
       sources: [s3Deploy.Source.asset(`application/frontend/dist/todo-application`)],
       destinationBucket: frontendBucket
     });
+
+    const todoItemTable = new dynamodb.Table(this, 'TodoApplicationTodoItemsTable', {
+      partitionKey: {
+        name: 'who',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'creationDate',
+        type: dynamodb.AttributeType.STRING
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
     bucketDeployment.node.addDependency(frontendBucket);
   }
 }
